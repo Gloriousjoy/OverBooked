4aa740dfa49ddc9fdbdd212c880dccfa1b25fac9
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-07 06:01:00 +0300

    adding images

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./About.css";
+import bookShelf from "../../images/bookShelf"
+
+const About = () => {
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src = {aboutImg} alt = "" />
+          </div>
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About OverBooked</h2>
+            <p className='fs-17'></p>
+            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
