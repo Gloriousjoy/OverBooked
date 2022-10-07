a93fe87bfa6007511c009e1b46ae23154d7781e5
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-07 22:38:53 +0300

    update about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,17 +7,17 @@ const About = () => {
     <section className='about'>
       <div className='container'>
         <div className='section-title'>
-          <h2>About</h2>
+          <h2>About OverBooked</h2>
         </div>
 
         <div className='about-content grid'>
           <div className='about-img'>
-            <img src ='src/images/bookShelf.jpg' alt = "" />
+            <img src ='src/images/library-img.jpg' alt = "" />
           </div>
           <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>About OverBooked</h2>
+            <h2 className='about-title fs-26 ls-1'> A bibliophile or bookworm is an individual who loves and frequently reads books. Here is a site for you;</h2>
             <p className='fs-17'>Inside a book's pages lie an amazing world where your guide is your imagination and the limit is how much time you can spare!</p>
-            <p className='fs-17'>Here is a book club for those who are willing to explore and nature your reading habbits!</p>
+            <p className='fs-17'>Here is a book club for those who are willing to explore and nature their reading habbits!</p>
           </div>
         </div>
       </div>
