98e024a73999fca1bda14162e2d0e974c11b0275
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-11 11:10:18 +0300

    Updates

---
+++ precache-manifest.bf323023f66865ef6fa3d3cbbf28002a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f835b3641b6c98ac2d915607ff806f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633c6d7fddd4a5a93618",
+    "url": "/static/css/main.284875dd.chunk.css"
+  },
+  {
+    "revision": "0981df0fae4340fe32c4",
+    "url": "/static/js/2.d6a52671.chunk.js"
+  },
+  {
+    "revision": "539e9828e086173d6c8e16e3602b9bcd",
+    "url": "/static/js/2.d6a52671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633c6d7fddd4a5a93618",
+    "url": "/static/js/main.47349444.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.47349444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c581e00d037a46e7b5",
+    "url": "/static/js/runtime-main.466e9062.js"
+  },
+  {
+    "revision": "9c4bbb21ac32475a2f3d8c55d2b7337d",
+    "url": "/static/media/cover_not_found.9c4bbb21.jpg"
+  }
+]);
